2e84f0d62e207e3a43298ab1ae5e7f5155c87875
Usiel13 <alberto990213@hotmail.com>
2023-06-14 00:01:51 -0600

    memoria tecnica

--- resources/views/proyectos/memoria_t.blade.php
+++ resources/views/proyectos/memoria_t.blade.php
@@ -1,12 +1,12 @@
 @extends('livewire-layout')
 
 @section('content')
+
 <section class="section">
     <div class="section-header">
         <h3 class="page__heading">Memoria Técnica</h3>
     </div>
 </section>
-
 <style>
     .form-row {
         display: flex;
@@ -15,7 +15,7 @@
     }
 
     .form-group {
-        flex-basis: calc(33.33% - 10px);
+        flex-basis: 100%;
     }
 
     .input-field {
@@ -73,33 +73,38 @@
         margin-top: 5px;
     }
 </style>
-
 <div style="background-color: #2E2D2F; border-radius: 30px; padding: 30px;">
     <h2 style="color: #FFFFFF; margin-bottom: 20px;">Formulario de Memoria Técnica</h2>
-    <form id="memoria-tecnica-form" action="guardar_memoria_tecnica.php" method="POST" enctype="multipart/form-data" onsubmit="return validateForm()">
+    <form id="memoria-tecnica-form"  onsubmit="return validateForm()">
         <div class="form-row">
-    
             <div class="form-group input-field">
                 <label for="descripcion_problematica">Descripción Problemática:</label>
-                <textarea name="descripcion_problematica" rows="5" cols="50"></textarea>
-            </div>
-            <div class="form-group input-field">
-                <label for="estado_arte">Estado del Arte:</label>
-                <input type="text" name="estado_arte">
+                <textarea name="descripcion_problematica" id="descripcion_problematica" rows="5" cols="50" oninput="countWords('descripcion_problematica', 300)"></textarea>
+                <p id="wordCount-descripcion_problematica" class="counter">0 palabras de 300</p>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group input-field">
+                <label for="estado_arte">Estado del Arte:</label>
+                <textarea name="estado_arte" id="estado_arte" rows="5" cols="50" oninput="countWords('estado_arte', 220)"></textarea>
+                <p id="wordCount-estado_arte" class="counter">0 palabras de 220</p>
+            </div>
+            <div class="form-group input-field">
                 <label for="descripcion_innovacion">Descripción de la Innovación:</label>
-                <input type="text" name="descripcion_innovacion">
+                <textarea name="descripcion_innovacion" id="descripcion_innovacion" rows="5" cols="50" oninput="countWords('descripcion_innovacion', 220)"></textarea>
+                <p id="wordCount-descripcion_innovacion" class="counter">0 palabras de 220</p>
             </div>
             <div class="form-group input-field">
                 <label for="propuesta_valor">Propuesta de Valor:</label>
-                <input type="text" name="propuesta_valor">
+                <textarea name="propuesta_valor" id="propuesta_valor" rows="5" cols="50" oninput="countWords('propuesta_valor', 220)"></textarea>
+                <p id="wordCount-propuesta_valor" class="counter">0 palabras de 220</p>
             </div>
-            <div class="form-group input-field">
+        </div>
+        <div class="form-row">
+            <div class="form-group input-field" style="flex-basis: 100%">
                 <label for="mercado_potencial">Mercado Potencial:</label>
-                <textarea name="mercado_potencial" rows="5" cols="50"></textarea>
+                <textarea name="mercado_potencial" id="mercado_potencial" rows="5" cols="50" oninput="countWords('mercado_potencial', 300)"></textarea>
+                <p id="wordCount-mercado_potencial" class="counter">0 palabras de 300</p>
             </div>
         </div>
         <div class="form-row">
@@ -107,27 +112,32 @@
                 <label for="imagen_mercado_potencial">Imagen Mercado Potencial:</label>
                 <input type="file" name="imagen_mercado_potencial">
             </div>
-            <div class="form-group input-field">
+            <div class="form-group input-field" style="flex-basis: 100%">
                 <label for="viabilidad_tecnica">Viabilidad Técnica:</label>
-                <textarea name="viabilidad_tecnica" rows="5" cols="50"></textarea>
-            </div>
-            <div class="form-group input-field">
-                <label for="imagen_viabilidad_tecnica">Imagen Viabilidad Técnica:</label>
-                <input type="file" name="imagen_viabilidad_tecnica">
+                <textarea name="viabilidad_tecnica" id="viabilidad_tecnica" rows="5" cols="50" oninput="countWords('viabilidad_tecnica', 300)"></textarea>
+                <p id="wordCount-viabilidad_tecnica" class="counter">0 palabras de 300</p>
             </div>
         </div>
         <div class="form-row">
             <div class="form-group input-field">
-                <label for="viabilidad_financiera">Viabilidad Financiera:</label>
-                <input type="text" name="viabilidad_financiera">
+                <label for="imagen_viabilidad_tecnica">Imagen Viabilidad Técnica:</label>
+                <input type="file" name="imagen_viabilidad_tecnica">
             </div>
+            <div class="form-group input-field">
+                <label for="viabilidad_financiera">Viabilidad Financiera:</label>
+                <textarea name="viabilidad_financiera" id="viabilidad_financiera" rows="5" cols="50" oninput="countWords('viabilidad_financiera', 220)"></textarea>
+                <p id="wordCount-viabilidad_financiera" class="counter">0 palabras de 220</p>
+            </div>
+        </div>
+        <div class="form-row">
             <div class="form-group input-field">
                 <label for="imagen_viabilidad_financiera">Imagen Viabilidad Financiera:</label>
                 <input type="file" name="imagen_viabilidad_financiera">
             </div>
             <div class="form-group input-field">
                 <label for="estrategia_propiedad_intelectual">Estrategia Propiedad Intelectual:</label>
-                <input type="text" name="estrategia_propiedad_intelectual">
+                <textarea name="estrategia_propiedad_intelectual" id="estrategia_propiedad_intelectual" rows="5" cols="50" oninput="countWords('estrategia_propiedad_intelectual', 110)"></textarea>
+                <p id="wordCount-estrategia_propiedad_intelectual" class="counter">0 palabras de 110</p>
             </div>
         </div>
         <div class="form-row">
@@ -135,47 +145,41 @@
                 <label for="imagen_propiedad_intelectual">Imagen Propiedad Intelectual:</label>
                 <input type="file" name="imagen_propiedad_intelectual">
             </div>
-            <div class="form-group input-field">
+            <div class="form-group input-field" style="flex-basis: 100%">
                 <label for="interpretacion_resultados">Interpretación de Resultados:</label>
-                <textarea name="interpretacion_resultados" rows="5" cols="50"></textarea>
+                <textarea name="interpretacion_resultados" id="interpretacion_resultados" rows="5" cols="50" oninput="countWords('interpretacion_resultados', 300)"></textarea>
+                <p id="wordCount-interpretacion_resultados" class="counter">0 palabras de 300</p>
             </div>
+        </div>
+        <div class="form-row">
             <div class="form-group input-field">
                 <label for="fuentes_consultadas">Fuentes Consultadas:</label>
-                <input type="text" name="fuentes_consultadas">
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group input-field">
-                <label for="archivo_adjunto">Archivo Adjunto:</label>
-                <input type="file" name="archivo_adjunto">
-            </div>
-            <div class="form-group input-field">
-                <label for="fecha">Fecha:</label>
-                <input type="text" name="fecha">
-            </div>
-            <div class="form-group input-field">
-                <label for="responsable">Responsable:</label>
-                <input type="text" name="responsable">
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group input-field">
-                <label for="estado">Estado:</label>
-                <input type="text" name="estado">
-            </div>
-            <div class="form-group input-field">
-                <label for="observaciones">Observaciones:</label>
-                <textarea name="observaciones" rows="5" cols="50"></textarea>
+                <textarea name="fuentes_consultadas" id="fuentes_consultadas" rows="5" cols="50" oninput="countWords('fuentes_consultadas', 110)"></textarea>
+                <p id="wordCount-fuentes_consultadas" class="counter">0 palabras de 110</p>
             </div>
         </div>
         <input class="submit-button" type="submit" value="Guardar">
     </form>
 </div>
-
 <script>
-    function validateForm() {
-        // Aquí puedes agregar tu lógica de validación si es necesario
-        return true;
+function validateForm() {
+    return true;
+}
+
+function countWords(inputId, maxWords) {
+    var mx = maxWords;
+    var text = document.getElementById(inputId).value;
+    var wordCount = text.trim().split(/\s+/).length;
+    var counter = document.getElementById('wordCount-' + inputId);
+    counter.innerText = wordCount + (wordCount === 1 ? ' palabra de ' + mx : ' palabras de ' + mx);
+
+    // Limitar a maxWords palabras
+    if (wordCount > maxWords) {
+        counter.style.color = 'red';
+        document.getElementById(inputId).value = text.split(/\s+/).slice(0, maxWords).join(' ');
+    } else {
+        counter.style.color = '#BEBEBE';
     }
+}
 </script>
 @endsection
--- resources/views/proyectos/proyectos.blade.php
+++ resources/views/proyectos/proyectos.blade.php
@@ -12,41 +12,41 @@
                     <div class="card-body">
                         <div class="row">
 
-                            <div class="col-md-4 col-xl-4">
+                            <div class="col-md-6 col-xl-6">
                                 <div class="card bg-c-gris order-card">
                                     <div class="card-block">
-                                        <h5>Ficha tecnica</h5>
-                                        <h2 class="text-right"><i class="fa fa-sheet-plastic f-left"></i><span></span></h2>
+                                        <h5>Ficha técnica</h5>
+                                        <h2 class="text-right"><i class="fa fa-sheet-plastic f-left fa-2x"></i><span></span></h2>
                                         <p class="m-b-0 text-right"><a href="{{ route('ficha_t.index') }}" class="text-white">Agregar</a></p>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="col-md-4 col-xl-4">
+                            <div class="col-md-6 col-xl-6">
                                 <div class="card bg-c-gris order-card">
                                     <div class="card-block">
                                         <h5>Requerimientos especiales</h5>
-                                        <h2 class="text-right"><i class="fa fa-square-check f-left"></i><span></span></h2>
+                                        <h2 class="text-right"><i class="fa fa-square-check f-left fa-2x"></i><span></span></h2>
                                         <p class="m-b-0 text-right"><a href="{{ route('requerimientos.index') }}" class="text-white">Agregar</a></p>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="col-md-4 col-xl-4">
+                            <div class="col-md-6 col-xl-6">
                                 <div class="card bg-c-gris order-card">
                                     <div class="card-block">
-                                        <h5>Memoria tecnica</h5>
-                                        <h2 class="text-right"><i class="fa fa-file-invoice f-left"></i><span></span></h2>
+                                        <h5>Memoria técnica</h5>
+                                        <h2 class="text-right"><i class="fa fa-file-invoice f-left fa-2x"></i><span></span></h2>
                                         <p class="m-b-0 text-right"><a href="{{ route('memoria_t.index') }}" class="text-white">Agregar</a></p>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="col-md-4 col-xl-4">
+                            <div class="col-md-6 col-xl-6">
                                 <div class="card bg-c-gris order-card">
                                     <div class="card-block">
                                         <h5>Modelo de negocios</h5>
-                                        <h2 class="text-right"><i class="fa fa-file-invoice-dollar f-left"></i><span></span></h2>
+                                        <h2 class="text-right"><i class="fa fa-file-invoice-dollar f-left fa-2x"></i><span></span></h2>
                                         <p class="m-b-0 text-right"><a href="#" class="text-white">Agregar</a></p>
                                     </div>
                                 </div>
@@ -57,8 +57,6 @@
                 </div>
             </div>
         </div>
-    </div>
-    </div>
     </div>
 </section>
 @endsection
