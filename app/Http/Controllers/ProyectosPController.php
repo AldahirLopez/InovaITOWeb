f4d43f83fc9400e85972396f64061da9569ed4ea
Usiel13 <alberto990213@hotmail.com>
2023-07-24 19:38:14 -0600

    proyectos aprobados y pend

--- app/Http/Controllers/ProyectosPController.php
+++ app/Http/Controllers/ProyectosPController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class RequerimientosController extends Controller
+class ProyectosPController extends Controller
 {
 
     public function index()
