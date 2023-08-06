dd26c9fdd766b4456d9cd3a63b342460cbfd2693
Aldahir Lopez Mac <hectoralr21@gmail.com>
2023-08-05 20:05:00 -0600

    cambios

--- app/Http/Controllers/proyectoController.php
+++ app/Http/Controllers/proyectoController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Estudiante;
 use App\Models\Persona;
 use App\Models\ProyectoParticipante;
+
 class proyectoController extends Controller
 {
 
@@ -14,45 +15,35 @@ class proyectoController extends Controller
 
 
     {
-        $ficha_tecnica_registrada=False;
-        $requerimientos_especiales_registrada=False;
+        $ficha_tecnica_registrada = False;
+        $requerimientos_especiales_registrada = False;
 
-        $memoria_tecnica_registrada=False;
-        $modelo_negocios_registrada=False;
-        
+        $memoria_tecnica_registrada = False;
+        $modelo_negocios_registrada = False;
+
         $usuario = session('usuario');
         $idpersona = $usuario->Id_persona;
         $persona = Estudiante::where('Id_persona', $idpersona)->first();
         $proyectoParticipante = ProyectoParticipante::where('Matricula', $persona->Matricula)->first();
-        $folioproyecto = $proyectoParticipante->Folio;
 
-        $proyecto=Proyecto::where('Folio',$folioproyecto)->first();
-        
-        if($proyecto->Id_fichaTecnica!=null){
-            $ficha_tecnica_registrada=True;
+        if ($proyectoParticipante !== null) {
+            $folioproyecto = $proyectoParticipante->Folio;
 
+            $proyecto = Proyecto::where('Folio', $folioproyecto)->first();
+
+            if ($proyecto->Id_fichaTecnica != null) {
+                $ficha_tecnica_registrada = true;
+            }
+
+            if ($proyecto->Id_memoriaTecnica != null) {
+                $memoria_tecnica_registrada = true;
+            }
+
+            if ($proyecto->Modelo_negocio != null) {
+                $modelo_negocios_registrada = true;
+            }
         }
 
-        if($proyecto->Id_memoriaTecnica!=null){
-            $memoria_tecnica_registrada=True;
-
-        }
-
-        if($proyecto->Modelo_negocio!=null){
-            $modelo_negocios_registrada=True;
-
-        }
-        
-        
-
-
-
-        return view('proyectos.proyectos',compact('ficha_tecnica_registrada','memoria_tecnica_registrada','modelo_negocios_registrada'));
+        return view('proyectos.proyectos', compact('ficha_tecnica_registrada', 'memoria_tecnica_registrada', 'modelo_negocios_registrada'));
     }
-
-
-
-
-
-   
 }
