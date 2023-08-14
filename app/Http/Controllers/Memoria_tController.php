8ea21576b095225c997b3e21232a05be6f586bd6
Aldahir Lopez Mac <hectoralr21@gmail.com>
2023-08-13 20:31:21 -0600

    memoria tecnica

--- app/Http/Controllers/Memoria_tController.php
+++ app/Http/Controllers/Memoria_tController.php
@@ -15,36 +15,33 @@ class Memoria_tController extends Controller
 
     public function store(Request $request)
     {
-        // Accede a los valores de los checkbox mediante el método input() del objeto Request.
-        $descripcion_problematica = request()->input('descripcion_problematica');
-        $estado_arte = request()->input('estado_arte');
-        $descripcion_innovacion = request()->input('descripcion_innovacion');
-        $propuesta_valor = request()->input('propuesta_valor');
-        $mercado_potencial = request()->input('mercado_potencial');
+        // Accede a los valores del formulario
+        $descripcion_problematica = $request->input('descripcion_problematica');
+        $estado_arte = $request->input('estado_arte');
+        $descripcion_innovacion = $request->input('descripcion_innovacion');
+        $propuesta_valor = $request->input('propuesta_valor');
+        $mercado_potencial = $request->input('mercado_potencial');
 
         $imagen_mercado_potencial = $request->file('imagen_mercado_potencial');
-        $imagen_mercado_potencialblobData = file_get_contents($imagen_mercado_potencial->getRealPath());
+        $imagen_mercado_potencial_blobData = $imagen_mercado_potencial ? file_get_contents($imagen_mercado_potencial->getRealPath()) : null;
 
-        $viabilidad_tecnica = request()->input('viabilidad_tecnica');
+        $viabilidad_tecnica = $request->input('viabilidad_tecnica');
 
-        $imagen_viabilidad_tecnica = $request->file('imagen_mercado_potencial');
-        $imagen_viabilidad_tecnicablobData = file_get_contents($imagen_viabilidad_tecnica->getRealPath());
+        $imagen_viabilidad_tecnica = $request->file('imagen_viabilidad_tecnica');
+        $imagen_viabilidad_tecnica_blobData = $imagen_viabilidad_tecnica ? file_get_contents($imagen_viabilidad_tecnica->getRealPath()) : null;
 
+        $viabilidad_financiera = $request->input('viabilidad_financiera');
 
-        $viabilidad_financiera = request()->input('viabilidad_financiera');
+        $imagen_viabilidad_financiera = $request->file('imagen_viabilidad_financiera');
+        $imagen_viabilidad_financiera_blobData = $imagen_viabilidad_financiera ? file_get_contents($imagen_viabilidad_financiera->getRealPath()) : null;
 
-        $imagen_viabilidad_financiera = $request->file('imagen_mercado_potencial');
-        $imagen_viabilidad_financierablobData = file_get_contents($imagen_viabilidad_financiera->getRealPath());
+        $estrategia_propiedad_intelectual = $request->input('estrategia_propiedad_intelectual');
 
+        $imagen_propiedad_intelectual = $request->file('imagen_propiedad_intelectual');
+        $imagen_propiedad_intelectual_blobData = $imagen_propiedad_intelectual ? file_get_contents($imagen_propiedad_intelectual->getRealPath()) : null;
 
-        $estrategia_propiedad_intelectual = request()->input('estrategia_propiedad_intelectual');
-
-
-        $imagen_propiedad_intelectual = $request->file('imagen_mercado_potencial');
-        $imagen_propiedad_intelectualblobData = file_get_contents($imagen_propiedad_intelectual->getRealPath());
-
-        $interpretacion_resultados = request()->input('interpretacion_resultados');
-        $fuentes_consultadas = request()->input('fuentes_consultadas');
+        $interpretacion_resultados = $request->input('interpretacion_resultados');
+        $fuentes_consultadas = $request->input('fuentes_consultadas');
 
         //Asiganar valores a Asesor
         $letra = 'MEMO';
@@ -63,9 +60,26 @@ class Memoria_tController extends Controller
 
         $memoria = new Memoria_t();
         $memoria->Id_memoriaTecnica = $nomenclatura;
+        $memoria->Descripcion_problematica = $descripcion_problematica;
+        $memoria->Estado_arte = $estado_arte;
+        $memoria->Descripcion_innovacion = $descripcion_innovacion;
+        $memoria->Propuesta_valor = $propuesta_valor;
+        $memoria->Mercado_potencial = $propuesta_valor;
+        $memoria->Imagen_mercadoPotencial = $imagen_mercado_potencial_blobData;
+        $memoria->Viabilidad_tecnica = $viabilidad_tecnica;
+        $memoria->Imagen_viabilidadTecnica = $imagen_viabilidad_tecnica_blobData;
+        $memoria->Viabilidad_financiera = $viabilidad_financiera;
+        $memoria->Imagen_viabilidadFinanciera = $imagen_viabilidad_financiera_blobData;
+        $memoria->Estrategia_propiedadIntelectual = $estrategia_propiedad_intelectual;
+        $memoria->Imagen_propiedadIntelectual = $imagen_propiedad_intelectual_blobData;
+        $memoria->Interpretacion_resultados = $interpretacion_resultados;
+        $memoria->Fuentes_consultadas = $fuentes_consultadas;
+
+        $memoria->save();
 
 
-         return redirect()->route('proyectos.index')->with('success', 'Memoria tecnica registrada ');
+
+
+        return redirect()->route('proyectos.index')->with('success', 'Memoria tecnica registrada ');
     }
-
 }
