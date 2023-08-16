b89b3f6e6dc2023d5d03e6ba2ccfaa6002d95df8
Usiel13 <alberto990213@hotmail.com>
2023-08-16 07:56:30 -0600

    cambios constancias

--- resources/views/constancias/pdf.blade.php
+++ resources/views/constancias/pdf.blade.php
@@ -4,12 +4,17 @@
     <title>Constancia</title>
 </head>
 <body>
-    <img src="{{ public_path('img/sep.png') }}" alt="Imagen 1" style="position: absolute; left: 0; top: 0; width: 100px; height: 100px;">
-    <img src="{{ public_path('img/tecNM.png') }}" alt="Imagen 2" style="position: absolute; right: 0; top: 0; width: 100px; height: 100px;">
+    <img src="{{ asset('img/sep.png') }}" alt="Imagen 1" style="position: absolute; left: 0; top: 0;  height: 100px;">
+    <img src="{{ asset('img/tecNM.png') }}" alt="Imagen 2" style="position: absolute; right: 0; top: 0;  height: 100px;">
+    <br>
+    <br>
 
-    <div style="text-align: center; margin-top: 120px;">
-        <h2>Generación de Constancias</h2>
-        <p>{{ $content }}</p>
+    <div style="text-align: center; margin-top: 140px;">
+        <h3>EL INSTITUTO TECNOLÓGICO NACIONAL DE MÉXICO </h3><h3>A TRAVES DEL INSTITUTO TECNOLÓGICO DE OAXACA</h3><h3> OTORGAN EL PRESENTE</h3>
+        <BR>
+        <h2>RECONOCIMIENTO</h2>
+        <h2>A</h2>
+        <h3>P</h3>
     </div>
 </body>
 </html>
